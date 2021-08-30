8f95462aaf7ed8077596a3118e457edd19a7cb7b
feeltheweb <89317240+feeltheweb@users.noreply.github.com>
2021-08-31 01:36:57 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 31 00:36:08 2021
+
+@author: aninditha
+"""
+def fib(n):
+    
+    a = 0
+    b = 1
+    
+    if n == 1:
+        print(a)
+        
+        
+    else:
+        print(a)
+        print(b)
+        
+        
+        for i in range(2, n):
+            pass
+        c = a + b
+        a = b
+        b = c 
+        print(c)
+           
+        
+fib(1)
+   
+
